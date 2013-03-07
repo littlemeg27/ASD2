68b566ae594f9d9b95ae593a9f63c2470b6bf310
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2013-03-06 23:11:44 -0500

    Changes small things in html and added 3 functions in js

--- Project1/js/main.js
+++ Project1/js/main.js
@@ -10,6 +10,77 @@
 		$('#addItem').on('pageinit', function()
 		{
 			
+			
+			var getData = function()
+			{
+				json = JSON.parse(reservation);		
+				
+				$.each(reservation, function(key)
+				{
+					console.log(key, reservation[key]);
+				});
+			};
+		
+			$('#saveData').on('click', function()
+			{
+				var key;
+				var id;
+	            
+	            if(!key)
+	            {
+	                id = Math.floor(Math.random()*1000001);    
+	            }
+	            
+	            else
+	            {
+	            	id = key;
+	            }
+	                
+	                
+		    var item                  = {};
+                item.lastName         =["Last Name:", $("#lastName").val()];
+                item.phoneNumber      =["Phone Number:", $("#phoneNumber").val()];
+                item.restaurant       =["Restaurant:", $("#restaurant").val()];    
+                item.numberOfPeople   =["Number Of People:", $("#numberOfPeople").val()];  
+                  
+                localStorage.setItem(id, JSON.stringify(item)); //Save data in not local storage: Use Stringify to convert our object to a string.
+                alert("Reservation Saved!");
+			});
+			
+			
+					
+			
 		});
+		
+		$('#showDataPage').on('click', function()
+		{
+			
+		});
+		
+		
+		/*
+		
+		Other things:
+		
+		$('makeItemLinks')
+		{
+		
+		//When the json loads make each item hold item links each
+		$('json').attr? //This might work? whole point is i want it to have links
+		//Delete and Edit Item links
+		//Could be something to do with the .attr 
+		
+		$('#deleteItem').on('click', function()
+		{
+		//code to delete item
+		}
+		
+		$('#editItem').on('click', function()
+		{
+		//code to edit item
+		}
+		
+		*/
+			
 		
 		
