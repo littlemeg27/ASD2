1bd55f93f71c5cccc3d16f2cecb2db858fd501bf
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2013-03-14 21:02:39 -0400

    changed from showDataPAge to dataPage like it should have been

--- Project1/js/main.js
+++ Project1/js/main.js
@@ -60,7 +60,7 @@
 		   
 		});
 		
-		$('#showDataPage').on('pageinit', function(key)//Since once i init into the page this shoud run, should i change it? 
+		$('#dataPage').on('pageinit', function(key)//Since once i init into the page this shoud run, should i change it? 
 														//Should i make it have a changepage and then change to this page?
 		{
 			 var item = localStorage.getItem(key);
