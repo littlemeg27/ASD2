1c64d7509eb0b97cb1deb556fb160230391c9ebc
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2013-03-22 15:43:03 -0400

    trying to fix issues

--- Project3/project3/app/_attachments/main.js
+++ Project3/project3/app/_attachments/main.js
@@ -10,13 +10,16 @@
                                     dataType: "json", //Getting JSON data, located in data.json   
                                     success: function(data) //Going to use dataCall for the name to call my data
                                      {  
-                                        $.each(data.rows, function(index, value)
+                                        $.each(data.rows, function(index, reservation)
                                         {
-                                            console.log(value);
-                                            var lastName         = value.value.lastName;
-                                            var numberOfPeople   = value.value.numberOfPeople;
-                                            var phoneNumber      = value.value.phoneNumber;
-                                            var restaurant       = value.value.restaurant;
+                                            var lastName         = reservation.value.lastName;
+                                            var numberOfPeople   = reservation.value.numberOfPeople;
+                                            var phoneNumber      = reservation.value.phoneNumber;
+                                            var restaurant       = reservation.value.restaurant;
+                                            console.log(lastName);
+                                            console.log(numberOfPeople);
+                                            console.log(phoneNumber);
+                                            console.log(restaurant);
                                             
                                             $('#reservationList').append(
                                             $("<ul>" + '<p>' + lastName[0] + " " + lastName[1] + '</p>' + "</ul>"));
