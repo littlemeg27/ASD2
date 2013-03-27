cd5f2ad0773d4b5b08962d808cc80074e4447298
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2013-03-26 22:54:35 -0400

    Added attachments to new cloudant project

---
+++ project4/app/_attachments/data.js
@@ -0,0 +1,123 @@
+var resurvation = 
+{
+      [
+      "lastName": 			["Last Name:", "Downey"],
+      "phoneNumber":		["Phone Number:", "803-412-7815"],
+      "restaurant":			["Restaurant", "Olive Garden"],
+      "Number Of People":   ["Number Of People", "more"]
+      },
+      {
+      "lastName": 			["Last Name:", "Stark"],
+      "phoneNumber":		["Phone Number:", "844-668-8263"],
+      "restaurant":			["Restaurant", "Applebee's"],
+      "Number Of People":   ["Number Of People", "six"]
+      },
+      {
+      "lastName": 			["Last Name:", "Hooks"],
+      "phoneNumber":		["Phone Number:", "811-062-5865"],
+      "restaurant":			["Restaurant", "Ruby Tuesday's"],
+      "Number Of People":   ["Number Of People", "three"]
+      },
+      {
+      "lastName": 			["Last Name:", "Early"],
+      "phoneNumber":		["Phone Number:", "855-440-8804"],
+      "restaurant":			["Restaurant", "Chili's"],
+      "Number Of People":   ["Number Of People", "two"]
+      },
+      {
+      "lastName": 			["Last Name:", "Ratcliff"],
+      "phoneNumber":		["Phone Number:", "822-829-1924"],
+      "restaurant":			["Restaurant", "LongHorn"],
+      "Number Of People":   ["Number Of People", "more"]
+      },
+      {
+      "lastName": 			["Last Name:", "Seals"],
+      "phoneNumber":		["Phone Number:", "803-705-5475"],
+      "restaurant":			["Restaurant", "Red Lobster"],
+      "Number Of People":   ["Number Of People", "more"]
+      },
+      {
+      "lastName": 			["Last Name:", "Ingram"],
+      "phoneNumber":		["Phone Number:", "803-414-0327"],
+      "restaurant":			["Restaurant", "Applebee's"],
+      "Number Of People":   ["Number Of People", "five"]
+      },
+      {
+      "lastName": 			["Last Name:", "Woodard"],
+      "phoneNumber":		["Phone Number:", "803-279-3420"],
+      "restaurant":			["Restaurant", "Olive Garden"],
+      "Number Of People":   ["Number Of People", "six"]
+      },
+      {
+      "lastName": 			["Last Name:", "Matthew"],
+      "phoneNumber":		["Phone Number:", "803-533-8731"],
+      "restaurant":			["Restaurant", "Ruby Tuesday's"],
+      "Number Of People":   ["Number Of People", "two"]
+      },
+      {
+      "lastName": 			["Last Name:", "Castillo"],
+      "phoneNumber":		["Phone Number:", "803-647-8212"],
+      "restaurant":			["Restaurant", "Red Lobster"],
+      "Number Of People":   ["Number Of People", "more"]
+      },
+      {
+      "lastName": 			["Last Name:", "Daugherty"],
+      "phoneNumber":		["Phone Number:", "803-301-9480"],
+      "restaurant":			["Restaurant", "Chili's"],
+      "Number Of People":   ["Number Of People", "six"]
+      },
+      {
+      "lastName": 			["Last Name:", "Mora"],
+      "phoneNumber":		["Phone Number:", "803-524-8684"],
+      "restaurant":			["Restaurant", "LongHorn"],
+      "Number Of People":   ["Number Of People", "two"]
+      },
+      {
+      "lastName": 			["Last Name:", "Wilder"],
+      "phoneNumber":		["Phone Number:", "803-999-4146"],
+      "restaurant":			["Restaurant", "Chili's"],
+      "Number Of People":   ["Number Of People", "five"]
+      },
+      {
+      "lastName": 			["Last Name:", "Pruett"],
+      "phoneNumber":		["Phone Number:", "803-226-8660"],
+      "restaurant":			["Restaurant", "Red Lobster"],
+      "Number Of People":   ["Number Of People", "five"]
+      },
+      {
+      "lastName": 			["Last Name:", "Cook"],
+      "phoneNumber":		["Phone Number:", "803-961-9532"],
+      "restaurant":			["Restaurant", "Ruby Tuesday's"],
+      "Number Of People":   ["Number Of People", "three"]
+      },
+      {
+      "lastName": 			["Last Name:", "Churchill"],
+      "phoneNumber":		["Phone Number:", "803-647-8212"],
+      "restaurant":			["Restaurant", "Applebee's"],
+      "Number Of People":   ["Number Of People", "more"]
+      },
+      {
+      "lastName": 			["Last Name:", "Norris"],
+      "phoneNumber":		["Phone Number:", "855-691-8585"],
+      "restaurant":			["Restaurant", "LongHorn"],
+      "Number Of People":   ["Number Of People", "four"]
+      },
+      {
+      "lastName": 			["Last Name:", "Ramos"],
+      "phoneNumber":		["Phone Number:", "899-053-9012"],
+      "restaurant":			["Restaurant", "Olive Garden"],
+      "Number Of People":   ["Number Of People", "three"]
+      },
+      {
+      "lastName": 			["Last Name:", "Douglass"],
+      "phoneNumber":		["Phone Number:", "822-232-0018"],
+      "restaurant":			["Restaurant", "Ruby Tuesday's"],
+      "Number Of People":   ["Number Of People", "two"]
+      },
+      {
+      "lastName": 			["Last Name:", "Tucker"],
+      "phoneNumber":		["Phone Number:", "803-412-7815"],
+      "restaurant":			["Restaurant", "LongHorn"],
+      "Number Of People":   ["Number Of People", "six"]
+      }
+]}
