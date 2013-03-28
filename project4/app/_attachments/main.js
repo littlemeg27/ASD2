2bdaaba364eefc63dbfeeb947b172340f975a2c6
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2013-03-28 18:38:25 -0400

    redid the main.js

--- project4/app/_attachments/main.js
+++ project4/app/_attachments/main.js
@@ -2,4 +2,30 @@
 //CouchDB Plugin Project
 //ASD 1303
 
+            $('#home').on("pageinit", function()
+            {
+            	$.couch.db("project4").view("app/reservation", 
+                {
+            		success: function(data)
+            		{
+            			console.log(data);//This works
+            			$('#reservationList').empty();
+            			$.each(data.rows, function(index, value)
+            			{
+            				var item = (value.value || value.doc);
+            				$('#reservationList').append(
+            				$('<li>').append(
+            				$('<a>').attr("href", "lastName.html?lastName=" + item.lastName)
+            						.text(item.lastName)
+            				)		
+            				);
+            			});
+            			$('#reservationList').listview('refresh');
+            		}
+                });
+            });
             
+            $('#lastName').on("pageinit", function()
+            {
+            	alert("im sitting in the lastName.html");
+            });
